033c983920047dc44fd81f4c434177c45aacec41
Oskar Lohse <oskar@baucampus.de>
2023-02-16 14:51:12 +0100

    test

--- config/app.php
+++ config/app.php
@@ -181,6 +181,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        NunoMaduro\Collision\Adapters\Laravel\CollisionServiceProvider::class,
 
         /*
          * Package Service Providers...
